af8d5d63e3eb62247f7620b4facb457826ebc306
kishoraswar <38615307+kishoraswar@users.noreply.github.com>
2022-07-13 08:44:01 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, kishor aswar");
+        System.out.println("Hello, DevOps22");
     }
 
 }
